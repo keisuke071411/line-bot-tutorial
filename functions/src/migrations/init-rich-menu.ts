0ae0a31424aae44b1d5c8ad2f6d00ff63f2dc226
keisuke071411 <i.love.football0714@gmail.com>
2022-04-17 08:52:20 +0900

    Update: initial up richmenu

--- functions/src/migrations/init-rich-menu.ts
+++ functions/src/migrations/init-rich-menu.ts
@@ -6,6 +6,10 @@ import { lineClient } from "~/utils/line"
 
   ; (async () => {
     try {
+      const oldRichMenuList = await lineClient.getRichMenuList()
+      oldRichMenuList.forEach(async (oldRichMenu) => {
+        await lineClient.deleteRichMenu(oldRichMenu.richMenuId)
+      })
 
       const richMenu: RichMenu = {
         "size": {
@@ -43,8 +47,47 @@ import { lineClient } from "~/utils/line"
         ]
       }
       const richMenuId = await lineClient.createRichMenu(richMenu)
+
+      const subRichMenu: RichMenu = {
+        "size": {
+          "width": 2500,
+          "height": 843
+        },
+        "selected": true,
+        "name": "リッチメニュー",
+        "chatBarText": "お知らせ",
+        "areas": [
+          {
+            "bounds": {
+              "x": 0,
+              "y": 0,
+              "width": 1254,
+              "height": 840
+            },
+            "action": {
+              "type": "message",
+              "text": "成功してるぜ"
+            }
+          },
+          {
+            "bounds": {
+              "x": 1246,
+              "y": 8,
+              "width": 1254,
+              "height": 835
+            },
+            "action": {
+              "type": "message",
+              "text": "リセット"
+            }
+          }
+        ]
+      }
+      const subRichMenuId = await lineClient.createRichMenu(subRichMenu)
+
       const buffer = fs.readFileSync(`${__dirname}/../../assets/menu.jpg`)
       await lineClient.setRichMenuImage(richMenuId, buffer)
+      await lineClient.setRichMenuImage(subRichMenuId, buffer)
 
       await lineClient.setDefaultRichMenu(richMenuId)
     } catch (err) {
