523d9b04f7a2c4873308664514fde6a63412cf57
keisuke071411 <i.love.football0714@gmail.com>
2022-04-16 16:18:34 +0900

    Add: Preparations for image analysis are complete

---
+++ functions/src/handlers/messages/index.ts
@@ -0,0 +1,22 @@
+import { MessageEvent } from '@line/bot-sdk'
+import { lineClient } from '~/utils/line'
+import { errorLogger } from '~/utils/util'
+import { msgOther } from '~line/notice-messages/other'
+import { messageImageHandler } from './image'
+
+
+export const messagesHandler = async (event: MessageEvent): Promise<void> => {
+  try {
+    switch (event.message.type) {
+      // case 'text':
+      //   return await messageTextHandler(event)
+      case 'image':
+        return await messageImageHandler(event)
+      default:
+        await lineClient.replyMessage(event.replyToken, msgOther)
+    }
+  } catch (err) {
+    errorLogger(err)
+    throw new Error('messages handler')
+  }
+}
